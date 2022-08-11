511a8d96b19a83c788bbadaa444d41d4183e4c33
Eki Alfani <ekialfani15@gmail.com>
2022-08-11 22:24:26 +0800

    add: interactivity by taking user input and displaying it on the screen

---
+++ assets/calculator.js
@@ -0,0 +1,30 @@
+const calculator = {
+	displayNumber: '0',
+	operator: null,
+	firstNumber: null,
+	isWaitForSecondNumber: false,
+}
+
+
+function updateDisplay() {
+	const display = document.querySelector('#display-number');
+
+	display.innerHTML = calculator.displayNumber;
+
+}
+
+function inputNumber(number){
+	calculator.displayNumber += number;
+}
+
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+	button.addEventListener('click', function(e){
+		const target = e.target;
+
+		inputNumber(target.textContent);
+		updateDisplay();		
+	})
+})
