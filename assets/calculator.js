b49ebcf15928670bccdf71cc62ac59d43d423a2a
Eki Alfani <ekialfani15@gmail.com>
2022-08-12 17:59:29 +0800

    add: functionality on the CE button to clear screen

--- assets/calculator.js
+++ assets/calculator.js
@@ -6,20 +6,30 @@ const calculator = {
 }
 
 
-function updateDisplay() {
+function updateDisplay(){
 	const display = document.querySelector('#display-number');
 
 	display.innerHTML = calculator.displayNumber;
+}
 
+
+function clearDisplay(){
+	calculator.displayNumber = '0';
+	calculator.operator = null;
+	calculator.firstNumber = null;
+	calculator.isWaitForSecondNumber = false;
 }
 
 function inputNumber(number){
-	if(number !== '.' && calculator.displayNumber == '0'){
+	const displayNumber = calculator.displayNumber;
+	const operator = calculator.operator;
+
+	if(number !== '.' && displayNumber === '0'){
 		calculator.displayNumber = number;
+
 	}else {
 		calculator.displayNumber += number;
 	}
-
 }
 
 
@@ -28,6 +38,13 @@ const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
 	button.addEventListener('click', function(e){
 		const target = e.target;
+
+		if(target.classList.contains('clear')){
+			clearDisplay();
+			updateDisplay();
+
+			return;
+		}
 
 		inputNumber(target.textContent);
 		updateDisplay();
