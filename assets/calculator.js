4426e3aff2138f58fc28277dd3b8f1168476b36e
Eki Alfani <ekialfani15@gmail.com>
2022-08-14 21:30:15 +0800

    fix: the operator button and equals.
    fixed the operator button and equals so that the calculator can perform
    the calculation process.

--- assets/calculator.js
+++ assets/calculator.js
@@ -22,7 +22,7 @@ function clearDisplay(){
 
 
 function inverseNumber(){
-	if(calculator.displayNumber == '0'){
+	if(calculator.displayNumber === '0'){
 		return;
 	}
 
@@ -30,16 +30,68 @@ function inverseNumber(){
 
 }
 
+
 function inputNumber(number){
 	const displayNumber = calculator.displayNumber;
 	const operator = calculator.operator;
 
-	if(number !== '.' && displayNumber === '0'){
+	if((number !== '.' && displayNumber === '0') || (displayNumber === operator)){
 		calculator.displayNumber = number;
 
 	}else {
 		calculator.displayNumber += number;
 	}
+}
+
+
+function handleOperator(operator){
+	if(!calculator.isWaitForSecondNumber){
+		calculator.firstNumber = calculator.displayNumber;
+		calculator.operator = operator;
+
+		calculator.displayNumber = operator;
+		calculator.isWaitForSecondNumber = true;
+
+	}else {
+		alert('The operator has been set');
+	}
+}
+
+
+function performCalculation(){
+	const firstNumber = Number(calculator.firstNumber);
+	const operator = calculator.operator;
+	const secondNumber = Number(calculator.displayNumber);
+
+	if(firstNumber === null || operator === null){
+		return;
+	}
+
+
+	let result = 0;
+
+	switch(operator){
+		case '+':
+			result = firstNumber + secondNumber;
+			break;
+
+		case '-':
+			result = firstNumber - secondNumber;
+			break;
+
+		case '×':
+			result = firstNumber * secondNumber;
+			break;
+
+		case '÷':
+			result = firstNumber / secondNumber;
+			break;
+
+		default:
+			result = firstNumber % secondNumber;
+	}
+
+	calculator.displayNumber = result;
 }
 
 
@@ -58,6 +110,20 @@ buttons.forEach(button => {
 
 		if(target.classList.contains('negative')){
 			inverseNumber();
+			updateDisplay();
+
+			return;
+		}
+
+		if(target.classList.contains('operator')){
+			handleOperator(target.textContent);
+			updateDisplay();
+
+			return;
+		}
+
+		if(target.classList.contains('equals')){
+			performCalculation();
 			updateDisplay();
 
 			return;
